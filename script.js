5f5e10c6dd4de0d136e90a95fec9ffb7f1fa2373
darrelwu <darrelwu@utexas.edu>
2022-09-01 17:23:41 -0500

    updated html and js

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ $(document).ready(function () {
         // creates a text
         var text = $(this).siblings('.entry').val();
         var time = $(this).parent().attr('id');
-    })
+    
 // saves the task input and timestamp into local storage
 localStorage.setItem(time, task);
-}
+});
 // gets items saved from local storage
 $("#hour09 .entry").val(localStorage.getItem("hour09"));
 $("#hour10 .entry").val(localStorage.getItem("hour10"));
@@ -32,5 +32,28 @@ $("#hour15 .entry").val(localStorage.getItem("hour15"));
 $("#hour16 .entry").val(localStorage.getItem("hour16"));
 $("#hour17 .entry").val(localStorage.getItem("hour17"));
 
-
+// updates time block color codes with if statements based on current time
+function scheduleUpdate() {
+    var currentHour = moment().hour();
+  
+    $(".time-block").each(function () {
+      var scheduleHour = parseInt($(this).attr("id").split("hour")[1]);
+  
+      if (scheduleHour < currentHour) {
+        $(this).addClass("past");
+      
+      } else if (scheduleHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+  
+      }
+    });
+   }
+  
+    scheduleUpdate();
 })
